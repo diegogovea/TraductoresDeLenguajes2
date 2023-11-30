260ba778f85b623cc472b1f3ff978d11f762dd3e
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2023-11-30 17:02:18 -0600

    Update analizadorSemmantico.py

--- Modulo 3/Analizador Semantico (Proyecto - 3)/analizadorSemmantico.py
+++ Modulo 3/Analizador Semantico (Proyecto - 3)/analizadorSemmantico.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ig-analizador-lexico.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import re
 import os
